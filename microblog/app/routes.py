51bb666aa831bff84b36a44516e3b59971f9f244
Sergio Serrano Bazán <sergio.serrano@madisonmk.com>
2018-05-29 20:52:58 +0200

    Chapter 2: Templates

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,7 +1,23 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Sergio'}
+    posts = [
+        {
+            'author': {'username': 'Robe'},
+            'body': 'Mira que camiseta mas chula!'
+        },
+        {
+            'author': {'username': 'Edgar'},
+            'body': 'Yo gasto la sintaxis es6'
+        },
+        {
+            'author': {'username': 'Sergio'},
+            'body': 'Se lo que me dices'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
