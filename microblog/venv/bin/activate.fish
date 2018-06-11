f69f2d062d642f34acc32915e9951218e59200a0
Sergio Serrano Bazán <sergio.serrano@madisonmk.com>
2018-06-11 12:09:22 +0200

    Chapter 4: Database

--- microblog/venv/bin/activate.fish
+++ microblog/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/palpatine222/Documents/myProjects/microblog/venv"
+set -gx VIRTUAL_ENV "/home/palpatine222/Documents/2018/microblog/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
