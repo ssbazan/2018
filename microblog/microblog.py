b04b3d1c82fd27d2dcf3027645dd9d58c009188a
Sergio Serrano Bazán <sergio.serrano@madisonmk.com>
2018-06-12 20:01:40 +0200

    Chapter 10: Email Support

--- microblog/microblog.py
+++ microblog/microblog.py
@@ -1,7 +1,14 @@
 from app import app, db
 from app.models import User, Post
+from flask_mail import Message
+from app import mail
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+    return {'db': db,
+            'User': User,
+            'Post': Post,
+            'mail': mail,
+            'Message': Message
+            }
